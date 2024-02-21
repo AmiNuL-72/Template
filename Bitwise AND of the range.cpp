294301779aedac40bac2dcb4fde4b377324bf66a
AmiNuL-72 <157211474+AmiNuL-72@users.noreply.github.com>
2024-02-22 02:33:09 +0600

    Create Bitwise AND of the range[l,r].cpp

---
+++ Bitwise AND of the range.cpp
@@ -0,0 +1,11 @@
+  /* Bitwise AND of the range [left, right] */
+
+    int rangeBitwiseAnd(int left, int right) {
+        int cnt=0;
+        while(left<right){
+            cnt++;
+            left/=2;
+            right/=2;
+        }
+        return (left<<cnt);
+    }
