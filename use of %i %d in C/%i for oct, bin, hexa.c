d1a3336444068a6a3a636c4bf22732cf15edecc2
AmiNuL-72 <157211474+AmiNuL-72@users.noreply.github.com>
2024-06-26 22:27:03 +0600

    Create %i for oct, bin, hexa.c

---
+++ use of %i %d in C/%i for oct, bin, hexa.c
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------------------
+
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%i",&x);
+    printf("%d",x);
+}
+
+/*
+    When you run this program, you can input values in different formats:
+    *Decimal: Input a regular decimal number, like 42.
+    *Octal: Input an octal number, which 
+           starts with 0, like 052 (which is 42 in decimal).
+    *Hexadecimal: Input a hexadecimal number, which 
+           starts with 0x or 0X, like 0x2A (which is also 42 in decimal).
+*/
+//-----------------------------------------------------------------------------
